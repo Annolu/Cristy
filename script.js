610ab19e5983c7faa9ad59d9720268360de3077f
Noemi <andreoli.noemi@gmail.com>
2017-02-23 18:40:11 +0000

    almost sorted top dropdown menu and search filter in the header

--- script.js
+++ script.js
@@ -13,25 +13,41 @@ $(document).ready(function(){
       
     });
     
-    $(".sorting-button").on('click', function(){
-        $(".dropdown-sorting li").toggleClass("open");
-    });
-    /*
-    $(".dropdown-toggle").on('mouseover', function(){
-        $('.dropdown-menu').toggleClass("menu-down");
-    });*/
+    // click on body makes the top dropdown menu disappear
+
+    var body= document.getElementsByTagName('body')[0];
+
+    body.addEventListener('click', function(event){
+        console.log(event.target.nextElementSibling)
+        console.log(event.target.classList.contains('dropdown-button'));
+        
+        if(event.target.classList.contains('dropdown-button')){
+            //event.target.nextElementSibling.children[0].classList.remove('open');
+            //$(".dropdown-menu .sorting li").removeClass("open");
+        }
+        else{
+            $(".dropdown-menu .sorting li").removeClass("open");
+        }
+    })
     
-
+    //make the top dropdown menu appear
+    
+    $(".dropdown-button").on('click', function(){
+        
+        var list= $(this).next()[0].children;              
+        var buttons = $(".dropdown-button").next();    
+        
+        //if click on the other buttons, the others menu disappear
+        for (lists of buttons){
+            var singleList =lists.children;
+            for (singleLi of singleList){
+                singleLi.classList.remove('open');
+            }
+        }
+        $(list).toggleClass("open");
+    });
+    
+    $(".sorting-button").on('click', function(){
+        $(".filter-section .sorting li").toggleClass("open");
+    });
 });
-
-/*
-var burgerContainer= document.getElementById('burger-container');
-
-burgerContainer.addEventListener('click', openBurger);
-
-function openBurger(event){
-    event.target.classList.add('open');
-    if(event.target.classList.open){
-        event.target.classList.remove('open');
-    }
-}*/
